f57e8fa47335590a0924aa198e4492c10e8372cc
cnrashmi <dkprasad00234@gmail.com>
2026-01-04 17:00:18 +0530

    add

--- API-validate-assignment-2/routes/courses/getAllCourses.js
+++ API-validate-assignment-2/routes/courses/getAllCourses.js
@@ -2,15 +2,39 @@ const router = require("express").Router();
 const dbConnect = require("../../database/db");
 
 router.get("/courses", async (req, res) => {
-  const conn = await dbConnect();
+  
   try {
-    const [rows] = await conn.execute("SELECT * FROM courses");
-    res.json({ success: true, data: rows });
+    const conn = await dbConnect();
+   let results;
+   if(req.query.title){
+        [results] = await connection.query(
+      "SELECT * FROM courses WHERE title LIKE ?",
+     [`%${req.query.title}%`]
+    );
+    }
+    else{
+      [results] = await connection.query(
+        "SELECT * FROM courses"
+      );
+    }
+    res.status(200).json({
+      success: true,
+      message: "Getting list of courses",
+      data: results,
+    });
   } catch (error) {
-    res.status(500).json({success: false,message: "Internal server error",
+    res.status(500).json({
+      sucess: false,
+      message: "internal server error",
       error: error.message,
     });
   }
 });
+    
 
 module.exports = router;
+
+
+
+
+    
